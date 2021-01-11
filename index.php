f26066fbd89ccabe1ef2614c421f1542081b3c68
Arcreuss <arcreuss74@gmail.com>
2021-01-11 17:17:33 +0100

    [ADD] Structure Box

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site théorie du complot</title>
 </head>
+<header>
+        <div class="banniere">
+            <img src="" alt="">
+        </div>
+</header>
 <body>
-    TEXT
-    
+        <div class="leftbox">
+
+        </div>
+        
+        <div class="rightbox">
+            <nav class="nav">
+                <ul>
+                    <li><a href="#">Actus</a></li>
+                    <li><a href="#">Histoire Populaire</a></li>
+                    <li><a href="#">Histoire</a></li>
+                </ul>
+            </nav>
+        </div>
 </body>
 </html>
