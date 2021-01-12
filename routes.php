634218086ee63f014b72eb402d5e2fc423d6a2ab
Roowka <lucasgoi746@gmail.com>
2021-01-12 10:37:40 +0100

    [ADD] version menu droite

--- routes.php
+++ routes.php
@@ -9,7 +9,6 @@
     if($_GET['p'] == "home")
         include_once('pages/home.php');
 
-<<<<<<< HEAD
     elseif($_GET['p'] == "aleatoire")
         include_once('pages/aleatoire.php');
 
@@ -17,15 +16,6 @@
         include_once('pages/submission.php');
 
     elseif($_GET['p'] == "apropos")
-=======
-    if($_GET['p'] == "aleatoire")
-        include_once('pages/aleatoire.php');
-
-    if($_GET['p'] == "submission")
-        include_once('pages/submission.php');
-
-    if($_GET['p'] == "apropos")
->>>>>>> 190a356ea83c133363a27028dec47564fd36766f
         include_once('pages/apropos.php');
     
 
