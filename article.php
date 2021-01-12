dc2ac379c389f5e9e09d3bf28b7be261201d1049
Arcreuss <arcreuss74@gmail.com>
2021-01-12 15:19:40 +0100

    [ADD] Article Automatique

--- article.php
+++ article.php
@@ -1,14 +1,31 @@
-<h3>
-        Les pyramides ont été construite par des extraterretres.
-</h3>
-<p>
-    Elon Musk a de nouveau sévi sur Twitter. Cette fois, l'excentrique milliardaire s'en est pris aux pyramides égyptiennes, attribuant leurs origines aux « extraterrestres ». L'Égypte en a profité pour inviter l'homme d'affaires américain à venir vérifier par lui-même, en visitant ses monuments antiques.
+<div class="article">
+    <?php
+    include_once("gestion data1.php")
+    ?>
 
-« Les pyramides ont évidemment été construites par des extraterrestres », avait déclaré vendredi l'excentrique milliardaire, reprenant un thème populaire parmi les conspirationnistes.
+    <div class="TopArticle">
+        <p>
+            <h1 id="TitreArticle"><?PHP echo $titre ?></h1>
+            <?PHP echo $auteur . ", le " . $datePublication ?>
+        </p>
+        
+    </div>
 
-La construction des titanesques pyramides de Gizeh a longtemps été entourée de mystère. Impossible pour certains d'attribuer l'œuvre de ces tombeaux à l'honneur des rois et reines d'Égypte aux ouvriers de l'époque, tant les moyens techniques semblaient limités.
+    <?PHP echo  "<img class='imgArticle' src=$lienImage alt=$comImage>" ?>
 
-Parmi les théories les plus farfelues sur ces édifices millénaires, figure la prédiction de la fin du monde. L'auteur conspirationniste David Meade aurait vu sur leurs murs et dans certains passages de la Bible la date du 23 septembre 2017 pour ce funeste événement. Une affirmation si souvent relayée qu'elle a dû être démentie par la Nasa dans un communiqué intitulé « Why the World Didn't End » (Pourquoi la fin du monde n'est-elle pas arrivée).
 
-Selon d'autres observateurs, le complexe pyramidal de Gizeh serait une « capsule temporelle » construite par les Atlantes, du nom de la civilisation mythique léguée par Platon. Sa fonction : voyager dans le temps et indiquer aux hommes du futur comment éviter de reproduire les erreurs commises par les générations précédentes.
-</p>
+    <p id="ParaArticle">
+        <?PHP
+        foreach ($paragraphe as $texte) {
+            echo $texte;
+        }
+        echo "<br/> </br>";
+
+        foreach ($source as $sr) {
+            echo "source: " . $sr;
+        }
+        ?>
+    </p>
+
+
+</div>
