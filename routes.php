147f975e30ba9cabbc33071e3a2ab26b2d1a6bd7
Arcreuss <arcreuss74@gmail.com>
2021-01-12 09:54:30 +0100

    [ADD] Modif if elseif

--- routes.php
+++ routes.php
@@ -9,6 +9,15 @@
     if($_GET['p'] == "home")
         include_once('pages/home.php');
 
+<<<<<<< HEAD
+    elseif($_GET['p'] == "aleatoire")
+        include_once('pages/aleatoire.php');
+
+    elseif($_GET['p'] == "submission")
+        include_once('pages/submission.php');
+
+    elseif($_GET['p'] == "apropos")
+=======
     if($_GET['p'] == "aleatoire")
         include_once('pages/aleatoire.php');
 
@@ -16,6 +25,7 @@
         include_once('pages/submission.php');
 
     if($_GET['p'] == "apropos")
+>>>>>>> 190a356ea83c133363a27028dec47564fd36766f
         include_once('pages/apropos.php');
     
 
