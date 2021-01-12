11695a7564d15536f1dd49b727d13f02bf49e82a
jean-michelsd5fsd65fsd5 <aymeric.pierre@etu.univ-smb.fr>
2021-01-12 16:44:34 +0100

    ajout page aleatoire

--- gestion data1.php
+++ gestion data1.php
@@ -1,18 +1,4 @@
 <?php  
-	
-	$_GET['p']="puce-dans-vaccin";
-
-	$file=fopen("data.csv", "r");
-
-	$article=array();
-	$i=0;
-	while (!feof($file)) {
-		$ligne=fgetcsv($file,0,";");
-		if($_GET['p']==$ligne[0]){
-			$article=$ligne;
-		}	
-
-	}
 	$taille=count($article);
 	if($taille!=0){
 		$titre=$article[0];
@@ -25,12 +11,9 @@
 		$source=array();
 		$commentaire=array();
 
-		$i=5;
-		while($article[$i]!="#source" && $i<=$taille){
-			$paragraphe[]=$article[$i];
-			$i++;
-		}
-		$i++;
+		
+		$paragraphe=str_getcsv($_POST['paragraphe'],"¬");
+		$i=6;
 		while($article[$i]!="#commentaire" && $i<=$taille){
 			$source[$article[$i]]=$article[$i+1];
 			$i+=2;
@@ -39,10 +22,7 @@
 		while($i<$taille){
 			$commentaire[]=array('username' => $article[$i],'date' => $article[$i+1],'commentaire' => $article[$i+2]);
 			$i+=3;
-
 		}
-
-		// include_once('test.php');
+		include_once('article.php');
 	}
-	fclose($file);
 ?>
--- pages/aleatoire.php
+++ pages/aleatoire.php
@@ -1,3 +1,28 @@
-<title>Aléatoire</title>
+<?php 
 
-Aléatoire a été lancé
+
+	$file=fopen("data.csv", "r");
+
+	$page=array();
+	$i=0;
+	while (!feof($file)) {
+		$ligne=fgetcsv($file,0,";");
+		if (count($ligne)>2){
+			$page[]=$ligne;
+		}
+	}
+	fclose($file);
+
+	$article=$page[rand (0,count($page)-1)];
+
+	include_once('gestion data1.php');
+
+	$localisation=$article[0];
+
+	header("Location: ?p=$localisation");
+    $_GET['p']= "$localisation";
+
+	
+
+
+ ?>
