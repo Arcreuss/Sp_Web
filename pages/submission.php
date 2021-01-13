6db36a590b6130c6d49ba27fb9906c7d161c32a5
jean-michelsd5fsd65fsd5 <aymeric.pierre@etu.univ-smb.fr>
2021-01-13 15:43:54 +0100

    amélioration du formulaire

--- pages/submission.php
+++ pages/submission.php
@@ -1,4 +1,11 @@
 <!doctype html>
+    <?PHP
+    if (! empty( $_POST))
+    {
+        include_once("submission-traitement.php");
+    }
+
+    ?>
 <html lang="fr">
 <head>
 <meta charset="utf-8" />
@@ -10,152 +17,87 @@
 <h1>Formulaire Théorie</h1>
 <form id="sub" method="post" action="">
 <div>
-<fieldset>
-<legend>Propositions</legend>
-<p class="choix"> <label>Que voulez vous nous partager ?</label>
+    <fieldset>
+    <legend>Votre article</legend>
+    
+    <p>
+        <label for="pseudo">Pseudo :</label> <strong><abbr title="required">*</abbr></strong>
+        <input required type="text" name="pseudo" id="pseudo" size="30"
+        maxlength="10" <?PHP if (isset ( $_POST['pseudo' ])) { $pseudo =
+        $_POST['pseudo' ]; echo "value =\"$pseudo\""; } ?> />
+    </p>
 
-<input type="radio" name="form" value="C"
-id="conseil" <?PHP if (isset ( $_POST['form' ]) && $_POST['form' ]=='C') echo "checked" ;
-?>/>
-<label for="conseil">Conseil</label>
+    <p>
+    <label for="mail">Mail :</label> <strong><abbr title="required">*</abbr></strong>
+    <input required type="email" name="mail" id="mail"
+    size="30" <?PHP if (isset ( $_POST['mail' ])) { $mail = $_POST['mail' ]; echo "value
+    =\"$mail\""; } ?> />
+    </p>
 
-<input type="radio" name="form" value="T"
-id="theorie" <?PHP if (isset ( $_POST['form' ]) && $_POST['form' ]=='T') echo "checked" ;
-?> />
-<label for="theorie">Théorie</label>
+    <p>
+    <label for="titre">Titre :</label> <strong><abbr title="required">*</abbr></strong>
+    <input required type="text" name="titre" id="titre" size="30"
+    maxlength="50" <?PHP if (isset ( $_POST['titre' ])) { $titre =
+    $_POST['titre' ]; echo "value =\"$titre\""; } ?> /></p>
+    </p>
 
-<input type="radio" name="form" value="A"
-id="autre" <?PHP if (isset ( $_POST['form' ]) && $_POST['form']=='A') echo "checked" ;
-?> />
-<label for="autre">Autres</label>
+    <p>
+    <label for="url">Image d'illustration</label>
+    <input type="url" name="url" id="url"
+           placeholder="https://images.unsplash.com/"
+           pattern="https://images.unsplash.com/.*" size="30" <?PHP if (isset ( $_POST['url' ])) { $url =
+    $_POST['url' ]; echo "value =\"$url\""; } ?> /></p>
+    </p>
 
-</p>
+    </select>
+    </p>
+    <p>
+    <label for="desc">Description de l'image</label> <br />
+    <textarea name="desc" id="desc" rows="5" cols="30">
+<?PHP if (isset ( $_POST['desc' ])) { echo $_POST['desc'];} 
+    ?></textarea>
+    </p>
+
+    <p>
+    <label for="prop"> Contenu de l'article</label> <br />
+    <textarea required name="prop" id="prop" rows="50" cols="100">
+<?PHP if (isset ( $_POST['prop'])) { echo $_POST['prop'];} ?></textarea>
+    <?php 
+                    for($i=0;$i<$_POST['+']+1;$i++){
+                        echo '<p>';
+                        echo "<label for=\"nomSource$i\">Nom de la source</label>";
+
+                        echo "<input";
+                        if (isset ( $_POST["nomSource$i" ])) { 
+                            $nom =$_POST["nomSource$i" ]; 
+                        echo " value =\"$nom\" ";}
+                        echo" type=\"text\" name=\"nomSource$i\" id=\"nomSource$i\">";
+
+                        echo "<label for=\"lienSource$i\">Lien de la source</label>";
+
+                        echo "<input";
+                        if (isset ( $_POST["lienSource$i"])) { 
+                            $lien =$_POST["lienSource$i"]; 
+                        echo " value =\"$lien\" ";}
+                        echo "  type=\"url\" name=\"lienSource$i\" id=\"lienSource$i\">";
+                    }
+
+                        $plus=$_POST['+']+1;
+                    echo "<button type=\"submit\" name=\"+\" value=\"$plus\">+</button>";
+
+                    
+                    if($_POST['+']>0){
+                        $plus=$_POST['+']-1;
+                        echo "<button type=\"submit\" name=\"+\" value=\"$plus\">-</button>";
+                    }
+                    echo '</p>';
+                ?>
+    </fieldset>
+<div>
+    <input type="submit" name="valider" id="valider" value="Envoyer"/>
+</div>
 <p>
-<label for="pseudo">Pseudo :</label> <strong><abbr title="required">*</abbr></strong>
-<input type="text" name="pseudo" id="pseudo" size="30"
-maxlength="10" <?PHP if (isset ( $_POST['pseudo' ])) { $pseudo =
-$_POST['pseudo' ]; echo "value =\"$pseudo\""; } ?> /></p>
 
-<p>
-<label for="mail">Mail :</label> <strong><abbr title="required">*</abbr></strong>
-<input type="email" name="mail" id="mail"
-size="30" <?PHP if (isset ( $_POST['mail' ])) { $mail = $_POST['mail' ]; echo "value
-=\"$mail\""; } ?> />
-</p>
-
-<p>
-<label for="titre">Titre :</label> <strong><abbr title="required">*</abbr></strong>
-<input type="text" name="titre" id="titre" size="30"
-maxlength="50" <?PHP if (isset ( $_POST['titre' ])) { $titre =
-$_POST['titre' ]; echo "value =\"$titre\""; } ?> /></p>
-</p>
-
-<p>
-<label for="url">Lien d'une illustration?</label>
-<input type="url" name="url" id="url"
-       placeholder="https://example.com"
-       pattern="https://.*" size="30"
-maxlength="40" <?PHP if (isset ( $_POST['url' ])) { $url =
-$_POST['url' ]; echo "value =\"$url\""; } ?> /></p>
-</p>
-
-</select>
-</p>
-<p>
-<label for="desc"> Une petite description de l'image ?</label> <br />
-<textarea name="desc" id="desc" rows="5" cols="30">
-<?PHP if (isset ( $_POST['desc' ])) { $adresse = $_POST['desc' ];
-echo "$desc"; } 
-?>
-</textarea>
-</p>
-
-<p>
-<label for="prop"> Proposition : ( tirez la fenetre dans le coin bas droite pour l'aggrandir )</label> <br />
-<textarea name="prop" id="prop" rows="5" cols="30">
-<?PHP if (isset ( $_POST['prop' ])) { $adresse = $_POST['prop' ];
-echo "$prop"; } 
-?>
-</textarea>
-<?php 
-                for($i=0;$i<$_POST['+']+1;$i++){
-                    echo '<p>';
-                    echo "<label for=\"nomSource$i\">Nom de la source</label>";
-
-                    echo "<input type=\"text\" name=\"nomSource$i\" id=\"nomSource$i\">";
-
-                    echo "<label for=\"lienSource$i\">Lien de la source</label>";
-
-                    echo "<input type=\"text\" name=\"lienSource$i\" id=\"lienSource$i\">";
-                }
-
-                    $plus=$_POST['+']+1;
-                echo "<button type=\"submit\" name=\"+\" value=\"$plus\">+</button>";
-
-                
-                if($_POST['+']>0){
-                    $plus=$_POST['+']-1;
-                    echo "<button type=\"submit\" name=\"+\" value=\"$plus\">-</button>";
-                }
-                echo '</p>';
-            ?>
-</fieldset>
-<fieldset>
-<legend>Infos autres</legend>
-<p>
-<label for="note">Notez notre site :</label>
-<input type="range" name="note" min="0" max="40" step="10" id="selec"
-<?PHP if (isset ( $_POST['note' ])) { $notes = $_POST['note' ]; echo "value =\"$notes\""; }
-else echo "value=\"0\" "; ?>
-/>
-</p>
-<p>
-<label for="nb">Votre age? :</label>
-<input type="number" name="nb" id="nb"
-<?PHP if (isset ( $_POST['nb' ])) { $nb = $_POST['nb' ]; echo "value =\"$nb\""; } ?>
-/>
-</p>
-<p class="choix">
-Types :<br />
-<?PHP
-$lestypes = array( "Paranormale", "Scientifiques", "Complotistes", "Autres" );
-foreach ( $lestypes as $type )
-{ echo "<input type=\"checkbox\" name=\"types[]\" id=\"$type\"
-value=\"$type\" ";
-if (isset ( $_POST['sports' ]) && in_array($type,$_POST['types' ])) echo "checked" ;
-echo "/> <label for=\"$type\">$type</label><br />" ;
-}
-?>
-</p>
-</fieldset>
-</div><div>
-<input type="submit" name="valider" id="valider" value="Envoyer"/></div>
-<p>
-<?PHP
-if (! empty( $_POST))
-{
-
-
-
-	$_POST['prop']=preg_replace("/\n/","¬", $_POST['prop']);
-
-    $_POST['titre']=preg_replace("/ /","-", $_POST['titre']);
-    $_POST['titre']=strtolower($_POST['titre']);
-
-
-	$lignesauvegarder=$_POST['titre'].';'.date('d-m-y').';'.$_POST['pseudo'].';'.$_POST['url'].';'.$_POST['desc'].';'.$_POST['prop'].';#source;';
-			$i=0;
-	while (array_key_exists("nomSource$i",$_POST)) {
-		$lignesauvegarder.=$_POST["nomSource$i"].";".$_POST["lienSource$i"].";";
-		$i++;
-	}
-	$lignesauvegarder.="#commentaire";
-
-	include_once("submission-traitement.php");
-
-}
-
-?>
 </p>
 </form>
 </body>
