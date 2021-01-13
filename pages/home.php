e41ec78b9674de53cf2876cee09c46ee62683bac
Arcreuss <arcreuss74@gmail.com>
2021-01-13 08:18:17 +0100

    [ADD] début home bug

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,22 @@
 <title>Home</title>
 
 Home a été lancé
+
+<div class="articleBox">
+    <?php
+    include_once("gestion data1.php");
+    $i=0;
+    while($i!=$taille){
+        echo "<div class='AllBoxArticle'>";
+        echo "<img class='imgBoxArticle' src=$lienImage alt=$comImage>";
+        echo "<p> <h1 id='TitreArticle'>";
+        echo "$titre </h1>";
+        echo "$auteur, le$datePublication </p>";
+        echo "</div>";
+        $i++;
+
+    }
+    
+    ?>
+
+</div>
