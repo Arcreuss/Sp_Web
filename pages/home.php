afe45231c23254cf108fc39f3b326371660d1cc2
YouRemJoestar <youneschaou@hotmail.fr>
2021-01-13 23:26:04 +0100

    Actualisation

--- pages/home.php
+++ pages/home.php
@@ -17,7 +17,9 @@ Home a été lancé
   text-align: left;
   width: 20%;
   margin-left: 150px;
+  margin-top:150px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 .my2Div {
   border: 5px outset red;
@@ -25,7 +27,9 @@ Home a été lancé
   text-align: left;
   width: 20%;
   margin-left: 50px;
+  margin-top:150px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 .my3Div {
   border: 5px outset red;
@@ -33,7 +37,9 @@ Home a été lancé
   text-align: left;
   width: 20%;
   margin-left: 50px;
+  margin-top:150px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 
 .my4Div {
@@ -44,6 +50,7 @@ Home a été lancé
   margin-left: 150px;
   margin-top: 50px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 
 .my5Div {
@@ -54,6 +61,7 @@ Home a été lancé
   margin-left: 50px;
   margin-top: 50px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 
 .my6Div {
@@ -64,13 +72,70 @@ Home a été lancé
   margin-left: 50px;
   margin-top: 50px;
   display: inline-block;
+  font-family: Verdana,Helvetica,Arial,sans-serif;
 }
 
-input {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  box-sizing: border-box;
+
+.glow-on-hover {
+    width: 220px;
+    height: 50px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    margin-bottom: 20px;
+    margin-left: 75px;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
 }
 
 </style>
@@ -80,45 +145,45 @@ input {
 <h1>Les populaires du moment :</h1>
 
 <div class="myDiv">
-  <h2>Le Titre</h2>
-  <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
+  <h2>"L'Apocalypse" 5G</h2>
+  <p>Une Nouvelle technologie a vus le jour récemment et il s'agit de la 5G! Il succède à la 4G et promet un accès à des débits bien meilleurs et bien plus performants de ceux de cette dernière avec des latences presques innexistantes et connexion rapide n'importe ou! Mais la provenance et les conséquences de cette nouvelle technologie est d'après certains très peu fiable et dangereuse !</p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici Pour Lire !</button>
 </div>
 
 <div class="my2Div">
-  <h2>Le Titre</h2>
-  <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
+  <h2>Un Virus aux origines vagues ?</h2>
+  <p>Le Covid-19 cache de nombreux mystères dont sa provenance qui n'a jamais été réellement prouvé encore aujourd'hui ! Provient-il de l'ingération d'une chauve-souris? D'un autre animal ? Personne ne sait et pourtant de plus en plus de personnes pensent que l'envers du décors est bien plus effreyante</p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici pour Lire !</button>
 </div>
 
 <div class="my3Div">
-  <h2>Le Titre</h2>
-  <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
+  <h2>Un vaccin particulier</h2>
+  <p>Récémment le vaccin Pfizer-BioNTech qui permet de combattre cette pathologie meurtrière qu'est le Covid-19 à été approuvé et commence déjà à être administré. Tandis que les états du monde le recommande pour combattre cette épidémie , une grande majorité s'oppose à cette vaccination pour différents motifs . La plus populaire d'entre elle? le fait que le vaccin contiendrais une puce assez spéciale... </p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici pour Lire !</button>
 </div>
 
 <div class="my4Div">
   <h2>Le Titre</h2>
   <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici pour Lire !</button>
 </div>
 
 <div class="my5Div">
   <h2>Le Titre</h2>
   <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici pour Lire !</button>
 </div>
 
 <div class="my6Div">
   <h2>Le Titre</h2>
   <p>Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion Ceci est une descritpion </p>
   <p>la Date , l'auteur etc</p>
-  <input type="submit" name="lire" id="lire" value="Lire la théorie"/>
+  <button class="glow-on-hover" type="button">Cliquez ici pour Lire !</button>
 </div>
 
 
