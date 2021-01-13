2dbdd7d212ff9ed945d3a244b0cff8eacd9d9ce0
Roowka <lucasgoi746@gmail.com>
2021-01-13 14:17:01 +0100

    [ADD] à propos

--- pages/apropos.php
+++ pages/apropos.php
@@ -1,3 +1,25 @@
-<title>A propos</title>
-
-A propos a été lancé
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pages/styleapropos.css"/>
+    <title>À propos</title>
+</head>
+<body>
+<h1>À propos</h1>
+<h2>Notre site</h2>
+<div class="desc">
+    <p>Notre site est né durant cette année 2020. Celle-ci étant pleine de problème nous nous sommes demandé s'il n'y avait pas des complots derrière tout ça. Bien évidemment nous ne sommes pas ici pour "dire la vérité" mais seulement pour parler des théories les plus fascinantes qui entourent notre monde.</p>
+</div>
+<h2>Où nous trouver ?</h2>
+<nav>
+    <ul>
+        <li>Instagram :</li>
+        <li>Snapchat :</li>
+        <li>Facebook :</li>
+        <li>MySpace :</li>
+    </ul>
+</nav>
+</body>
+</html>
